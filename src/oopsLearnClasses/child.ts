6734eb10d9d76f87be5f0369fd82db53b007f2ba
Deepak <102788188+daradeepak123@users.noreply.github.com>
2026-07-20 19:54:06 +0530

    20-07 code with OOPS

---
+++ src/oopsLearnClasses/child.ts
@@ -0,0 +1,7 @@
+import { XYZ,XYZ1 } from "./parent";
+
+let oo=new XYZ()
+let o1=new XYZ1()
+
+console.log(oo.aa)
+console.log(o1.aa)
---
+++ src/oopsLearnClasses/parent.ts
@@ -0,0 +1,23 @@
+
+
+
+
+
+export class XYZ {
+
+    aa: number = 500
+    static bb: number = 400
+}
+
+
+export class XYZ1 {
+
+    aa: number = 500
+    static bb: number = 400
+}
+
+
+let o = new XYZ()
+
+// console.log(o.aa)
+// console.log(XYZ.bb)
